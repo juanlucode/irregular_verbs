28b41f7ed2290048fc4f8b6870ceb377c58648bb
juanlucode <juanluis.garciarodriguez@gmail.com>
2022-02-10 13:43:28 +0100

    refactoring ControllerFactory

--- java_project/iverbsdesktop/src/main/java/io.github.juanlucode.iverbsdesktop/io/github/juanlucode/iverbsdesktop/controllers/ControllerFactory.java
+++ java_project/iverbsdesktop/src/main/java/io.github.juanlucode.iverbsdesktop/io/github/juanlucode/iverbsdesktop/controllers/ControllerFactory.java
@@ -5,23 +5,23 @@ import io.github.juanlucode.iverbsdesktop.front.View;
 
 /**
  * Factory Pattern to get controllers
+ *
  * @author juanluis
  *
  */
 public final class ControllerFactory {
-	
-	public static Controller get(MainAppFx app, View view) {
-        switch(view) {
-        case MENU:
-        	return new MenuController(app);
-        case QUESTION:
-        	return new QuestionController(app);
-        case RESULT:
-        	return new ResultController(app);
-        default:
-        	return null;
-        }
-    }		
 
+    public static Controller get(MainAppFx _app, View _view) {
+        switch (_view) {
+            case MENU:
+                return new MenuController(_app);
+            case QUESTION:
+                return new QuestionController(_app);
+            case RESULT:
+                return new ResultController(_app);
+            default:
+                return null;
+        }
+    }
 
 }
