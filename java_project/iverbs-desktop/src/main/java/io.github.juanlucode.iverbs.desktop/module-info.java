22892b93a8d048ab2a7ade18ae080785d4fafdf4
juanluis.garciar <juanluis.garciar@juntaex.es>
2023-01-04 14:05:48 +0100

    jar packaging with shade-maven-plugin

--- java_project/iverbs-desktop/src/main/java/io.github.juanlucode.iverbs.desktop/module-info.java
+++ java_project/iverbs-desktop/src/main/java/io.github.juanlucode.iverbs.desktop/module-info.java
@@ -1,13 +1,14 @@
 module io.github.juanlucode.iverbs.desktop {
 	
-	requires transitive javafx.graphics;
+	//requires transitive javafx.graphics;
 	requires javafx.controls;
 	requires javafx.fxml;
-	
+
+
 	requires transitive io.github.juanlucode.iverbs.core;
         
 	
-	opens io.github.juanlucode.iverbs.desktop.front to javafx.fxml;
+	opens io.github.juanlucode.iverbs.desktop.front to javafx.graphics, javafx.controls;
 	opens io.github.juanlucode.iverbs.desktop.controllers to javafx.fxml;
 	exports io.github.juanlucode.iverbs.desktop.front;
 	exports io.github.juanlucode.iverbs.desktop.controllers;
